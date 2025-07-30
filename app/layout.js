83113e79f1990ef690c19c3d2bd6b8f1155d044e
dnyandeoShekade <shekadednyande@gmail.com>
2025-07-30 18:25:23 +0530

    third commet

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/components/Comman/Navbar";
+import  Navbar   from "@/components/Comman/Navbar";
 import Footer from "@/components/Comman/Footer";
 
 const geistSans = Geist({
--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
-import { AboutMeSection } from "@/components/Homepage/About";
-import { HeroSection } from "@/components/Homepage/Hero";
-import { WorkExperienceSection } from "@/components/Homepage/Project";
+import  AboutMeSection  from "@/components/Homepage/About";
+import  HeroSection  from "@/components/Homepage/Hero";
+import  WorkExperienceSection  from "@/components/Homepage/Project";
 import Image from "next/image";
 
 export default function Home() {
