836cff378ae823ef794d7b686105ddbec51059b9
dnyandeoShekade <shekadednyande@gmail.com>
2025-07-30 18:05:53 +0530

    second comment

--- Components/Homepage/Hero.js
+++ Components/Homepage/Hero.js
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button"
+import Button from '@/components/ui/button'
 import { FiArrowDown, FiArrowUpRight } from "react-icons/fi"
 import Link from "next/link"
 
--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
-import { AboutMeSection } from "@/Components/Homepage/About";
-import { HeroSection } from "@/Components/Homepage/Hero";
-import { WorkExperienceSection } from "@/Components/Homepage/Project";
+import { AboutMeSection } from "@/components/Homepage/About";
+import { HeroSection } from "@/components/Homepage/Hero";
+import { WorkExperienceSection } from "@/components/Homepage/Project";
 import Image from "next/image";
 
 export default function Home() {
