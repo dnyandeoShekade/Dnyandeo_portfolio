99409a5a350e00c25b18619cf1e5e25863319e15
dnyandeoShekade <shekadednyande@gmail.com>
2025-07-31 14:26:23 +0530

    update file name

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import  AboutMeSection  from "@/components/homepage/About";
-import  HeroSection  from "@/components/homepage/Hero";
+import HeroSection from "@/components/homepage/HeroSection";
 import  WorkExperienceSection  from "@/components/homepage/Project";
 import Image from "next/image";
 
